f6f388e3194ac3059cf815fba180250f6ab3a730
orm-viking <burzum.rulit@gmail.com>
2022-11-15 23:00:18 +0400

    Ёще одна ката.

--- main.py
+++ main.py
@@ -193,3 +193,31 @@ def lovefunc(flower1, flower2):
         return False
     else:
         return True
+
+
+# Grader
+
+"""
+Create a function that takes a number as an argument and returns a grade based on that number.
+
+Score	Grade
+Anything greater than 1 or less than 0.6	"F"
+0.9 or greater	"A"
+0.8 or greater	"B"
+0.7 or greater	"C"
+0.6 or greater	"D"
+
+"""
+
+
+def grader(score):
+    if score > 1 or score < 0.6:
+        return "F"
+    elif 0.6 <= score < 0.7:
+        return "D"
+    elif 0.7 <= score < 0.8:
+        return "C"
+    elif 0.8 <= score < 0.9:
+        return "B"
+    elif 0.9 <= score:
+        return "A"
